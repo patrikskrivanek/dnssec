48fcce973a9be373bb0f028496fdc0663064b053
Patrik Skřivánek <patrik.skrivanek@gmail.com>
2019-08-18 13:39:17 +0200

    setup update

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import setuptools
 import codecs
 
 with codecs.open('README.md', 'r', 'utf-8') as f:
@@ -6,16 +7,23 @@ with codecs.open('README.md', 'r', 'utf-8') as f:
 
 setup(
     name="dnssec",
-    version="v0.0.0",
+    version="v1.0.0",
+    scripts=['dnssec'],
     url="https://github.com/patrikskrivanek/dnssec",
     download_url="https://github.com/patrikskrivanek/dnssec/releases",
     author="Patrik Skřivánek",
     author_email="kriegsmarine1995@gmail.com",
     description="A Python tool for checking a state of domains signed by dnssec",
     long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    license="MIT",
     keywords="dnssec validation",
     python_requires=">=3.6",
-    license="MIT",
+    install_requires=[
+        'dnspython',
+        'pycryptodomex',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
